c3b816b68948ff66c00b65f8d9fe376412455abc
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-12 18:31:54 +0200

    feature implement prompt

---
+++ prompt.js
@@ -0,0 +1,7 @@
+//vytvořeí promptů pro zadání jména + příjmení a věku a naplnění do proměnných
+
+let celeJmeno = prompt("Zadej jméno a příjmení");
+let vek = prompt("Zadej věk", 18); //vychozí hodnota
+
+//vypsání výsledků pomocí alert()
+alert(`Jmenuješ se ${celeJmeno} a je ti ${vek} let.`)
